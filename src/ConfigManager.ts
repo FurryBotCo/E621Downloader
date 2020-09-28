cb89bbb1cfb4aad3d7359bf6d41fbe1b92c8ea22
Donovan_DMC <dwddaniels262@gmail.com>
2020-09-27 21:35:29 -0500

    default config file

--- src/ConfigManager.ts
+++ src/ConfigManager.ts
@@ -15,10 +15,24 @@ interface Config {
 
 export default class ConfigManager {
 	static ROOT_DIR = path.resolve(`${__dirname}/../${__filename.endsWith("ts") ? "" : "../"}`);
-	static CONFIG_LOCATION = path.resolve(`${ConfigManager.ROOT_DIR}/config.yaml`);
-	static loadFile() { return fs.readFileSync(this.CONFIG_LOCATION); }
+	static LOCATION = path.resolve(`${process.env.APPDATA || (process.platform === "darwin" ? process.env.HOME + "/Library/Preferences" : process.env.HOME + "/.local/share")}/E621Downloader/config.yaml`);
+	static DEFAULT_LOCATION = path.resolve(`${ConfigManager.ROOT_DIR}/config.default.yaml`);
+	static loadFile() { return fs.readFileSync(this.LOCATION); }
+
+	static setup() {
+		if (!fs.existsSync(this.LOCATION)) {
+			const d = path.dirname(this.LOCATION);
+			if (!fs.existsSync(d)) {
+				console.log(`Creating configuration directory "${d}"`);
+				fs.mkdirpSync(d);
+			}
+			console.log(`Copying default config file "${this.DEFAULT_LOCATION}" to "${this.LOCATION}"`);
+			fs.copyFileSync(this.DEFAULT_LOCATION, this.LOCATION);
+		}
+	}
 
 	static get() {
+		this.setup();
 		const f = this.loadFile();
 		const c = YAML.safeLoad(f.toString()) as Config;
 		c.saveDirectory = path.resolve(c.saveDirectory.startsWith(".") ? `${this.ROOT_DIR}/${c.saveDirectory}` : c.saveDirectory);
@@ -30,6 +44,6 @@ export default class ConfigManager {
 		const v = this.get();
 		const c = { ...v, ...values };
 		if (JSON.stringify(v) === JSON.stringify(c)) return;
-		fs.writeFileSync(this.CONFIG_LOCATION, YAML.safeDump(c));
+		fs.writeFileSync(this.LOCATION, YAML.safeDump(c));
 	}
 }
